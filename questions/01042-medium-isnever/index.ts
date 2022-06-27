77682783b28f755ce7456c5d43493c7d9a1aa583
Gabriel Moraes <gabrielhm2001@gmail.com>
2022-06-27 02:31:09 -0300

    01042-medium-isnever

---
+++ questions/01042-medium-isnever/index.ts
@@ -0,0 +1,14 @@
+export type IsNever<T> = [T] extends [never] ? true : false
+
+/* _______________ Test Cases _______________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<IsNever<never>, true>>,
+  Expect<Equal<IsNever<never | string>, false>>,
+  Expect<Equal<IsNever<''>, false>>,
+  Expect<Equal<IsNever<undefined>, false>>,
+  Expect<Equal<IsNever<null>, false>>,
+  Expect<Equal<IsNever<[]>, false>>,
+  Expect<Equal<IsNever<{}>, false>>
+]
