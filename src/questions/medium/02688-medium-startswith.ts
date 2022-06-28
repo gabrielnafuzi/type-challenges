0efd4f244e4b20f55d8b6aba7d3ae12e283a5955
Gabriel Moraes <gabrielhm2001@gmail.com>
2022-06-27 22:43:10 -0300

    02688-medium-startswith

---
+++ src/questions/medium/02688-medium-startswith.ts
@@ -0,0 +1,15 @@
+export type StartsWith<
+  T extends string,
+  U extends string
+> = T extends `${U}${infer _Rest}` ? true : false
+
+/* _______________ Test Cases _______________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<StartsWith<'abc', 'ac'>, false>>,
+  Expect<Equal<StartsWith<'abc', 'ab'>, true>>,
+  Expect<Equal<StartsWith<'abc', 'abcd'>, false>>,
+  Expect<Equal<StartsWith<'abc', ''>, true>>,
+  Expect<Equal<StartsWith<'abc', ' '>, false>>
+]
