3d9a3588dc67619fb80f4479251fc109bb4e5824
Gabriel Moraes <gabrielhm2001@gmail.com>
2022-06-27 21:38:43 -0300

    00533-easy-concat

---
+++ src/questions/easy/00533-easy-concat.ts
@@ -0,0 +1,16 @@
+export type Concat<T extends any[], U extends any[]> = [...T, ...U]
+
+/* _______________ Test Cases _______________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<Concat<[], []>, []>>,
+  Expect<Equal<Concat<[], [1]>, [1]>>,
+  Expect<Equal<Concat<[1, 2], [3, 4]>, [1, 2, 3, 4]>>,
+  Expect<
+    Equal<
+      Concat<['1', 2, '3'], [false, boolean, '4']>,
+      ['1', 2, '3', false, boolean, '4']
+    >
+  >
+]
